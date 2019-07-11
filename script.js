491c8f43dba49397afe577b35af60fc7e2a39e48
ryansims265 <44000703+ryansims265@users.noreply.github.com>
2019-07-11 19:18:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $("#submit-button").on("click", function(event) {
 console.log("testing");
 });
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA2dI2zWOEsFCpHiCDs5T4200ax98VJRb8",
+    authDomain: "anotherone-631a5.firebaseapp.com",
+    databaseURL: "https://anotherone-631a5.firebaseio.com",
+    projectId: "anotherone-631a5",
+    storageBucket: "",
+    messagingSenderId: "710450884721",
+    appId: "1:710450884721:web:f469249e7ff2ce00"
+  };
+  firebase.initializeApp(firebaseConfig);
